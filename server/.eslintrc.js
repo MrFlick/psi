3214827c170d403ee87ab7d67b98bdb6a7fa6067
Matthew Flickinger <mflick@umich.edu>
2018-10-17 00:42:18 -0400

    Add eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {"node": true},
+    "rules": {
+      "no-console": 0
+     },
+    "extends": [
+      "eslint:recommended"
+    ],
+    "parser": "babel-eslint"
+  };
