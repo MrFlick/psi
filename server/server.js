92db6e814d4b1f639ea3312ffbbf4dbbeeff57ad
Matthew Flickinger <mflick@umich.edu>
2018-10-17 00:17:12 -0400

    Add API router

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const api = require("./routes/api")
+
 app.use(express.static('public'))
 app.use(express.static('react-build'))
+
+app.use("/api", api)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
