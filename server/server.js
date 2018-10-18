a5aae5ae225f98c423dcb4c4ea8ac485f92c610f
Matthew Flickinger <mflick@umich.edu>
2018-10-17 21:50:55 -0400

    Use PUG templates in server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-var Sequelize = require("sequelize")
 
+app.set("views", "./server/views");
+app.set("view engine", "pug");
+
+var Sequelize = require("sequelize")
 var sequelize = new Sequelize("sqlite:db/data.sqlite3", {
     operatorsAliases: true
 });
@@ -12,6 +15,10 @@ const api = require("./routes/api")(sequelize)
 app.use(express.static('public'))
 app.use(express.static('react-build'))
 app.use("/api", api)
+
+app.get("/", function(req, res) {
+	res.render("index");
+});
 
 sequelize.sync().then(function() {
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
