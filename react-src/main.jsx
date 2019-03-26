01c4a3301e3afb1d75b373cc4c812af993c2d5f6
Matthew Flickinger <mflick@umich.edu>
2019-03-25 23:56:31 -0400

    Swap out react router for root

--- react-src/main.jsx
+++ react-src/main.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomeView from './HomeView';
 import TermView from './TermView';
 import ClassView from './ClassView';
 import StudentView from './StudentView';
-import camelCase from './CamelCase';
+
+function ClassRoute({ match }) {
+  return (<ClassView classId={match.params.classId} />);
+}
+ClassRoute.propTypes = {
+  match: PropTypes.shape({ params: PropTypes.any }).isRequired,
+};
+
+function TermRoute({ match }) {
+  return (<TermView termId={match.params.termId} />);
+}
+TermRoute.propTypes = {
+  match: PropTypes.shape({ params: PropTypes.any }).isRequired,
+};
+
+function StudentRoute({ match }) {
+  return (<StudentView personId={match.params.personId} />);
+}
+StudentRoute.propTypes = {
+  match: PropTypes.shape({ params: PropTypes.any }).isRequired,
+};
+
+function HomeRoute() {
+  return (<HomeView />);
+}
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/class/:classId" component={ClassRoute} />
+        <Route path="/term/:termId" component={TermRoute} />
+        <Route path="/student/:personId" component={StudentRoute} />
+        <Route path="/" component={HomeRoute} />
+      </Switch>
+    </Router>
+  );
+}
 
 const root = document.getElementById('root');
-const view = (root.dataset && root.dataset.view) || 'home';
-const props = camelCase((root.dataset && root.dataset.props
-  && JSON.parse(root.dataset.props)) || {});
-
-switch (view) {
-  case 'class':
-    ReactDOM.render(React.cloneElement(<ClassView />, props), root);
-    break;
-  case 'term':
-    ReactDOM.render(React.cloneElement(<TermView />, props), root);
-    break;
-  case 'student':
-    ReactDOM.render(React.cloneElement(<StudentView />, props), root);
-    break;
-  default:
-    ReactDOM.render(React.cloneElement(<HomeView />, props), root);
-}
+ReactDOM.render(<App />, root);
